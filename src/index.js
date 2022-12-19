5122fad237b27a6bc18d588d1e77ab8960390234
Lyman Hurd <lyman.hurd@gmail.com>
2022-12-19 12:15:03 -0500

    Apears stable with computer player implemented

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React, { useRef, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+
+import { computerMove, isDead, isStart, humanMove, isGameOver } from './logic';
 
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
+/**
+ * Constants that determine the canvas dimensions for the individual boards
+ * Note that each column (and row) has three squares but two lines
+ */
+const LINE_WIDTH = 3;
+const SQUARE_WIDTH = 60;
+const SQUARE_WITH_BDY = SQUARE_WIDTH + LINE_WIDTH;
+const CANVAS_SIZE = 3 * SQUARE_WIDTH + 2 * LINE_WIDTH;
+
 function StartButton(props) {
-  return (<Button variant="outline-primary" size="lg" onClick={props.onClick}>
+  return (<Button variant={props.variant} size="lg" onClick={props.onClick}>
       {props.startLabel}
       </Button>);
 }
@@ -38,7 +50,7 @@ function Board(props) {
     });
   }, [props.contents]);
     
-  return (<canvas ref={canvasRef} width={186} height={186} {...props}>
+  return (<canvas ref={canvasRef} width={CANVAS_SIZE} height={CANVAS_SIZE} {...props}>
             Your browser does not support HTML5.
           </canvas>);
 }
@@ -53,27 +65,42 @@ function Scores(props) {
 
 class Game extends React.Component {
   constructor(props) {
-      super(props);
+    super(props);
       this.state = {
-        computerWins: 0,
-        humanWins: 0,
-        numBoards: 1,
-        moveNumber: 0,
-        winner: "-",
+        stats: {
+          computerWins: 0,
+          humanWins: 0,
+        },
         boards: [
           '---------',
         ],
-        dead: [
-          false,
-        ],
       };
+  }
+
+  componentDidMount() {
+    const boardsJson = sessionStorage.getItem("notaktoBoards");
+    const savedBoards = JSON.parse(boardsJson);
+    const statsJson = sessionStorage.getItem("notaktoStats");
+    const savedStats = JSON.parse(statsJson);
+    if (savedBoards && savedStats) {
+      this.setState({
+        boards: savedBoards, 
+        stats: savedStats
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevStates) {
+    const boardsJson = JSON.stringify(this.state.boards);
+    sessionStorage.setItem("notaktoBoards", boardsJson);
+    const statsJson = JSON.stringify(this.state.stats);
+    sessionStorage.setItem("notaktoStats", statsJson);    
   }
 
   plusClickHandler() {
     const newBoards = this.state.boards.concat(["---------"]);
     this.setState({
       boards: newBoards,
-      dead: new Array(newBoards.length).fill(false),
     });
   }
 
@@ -82,84 +109,113 @@ class Game extends React.Component {
       const newBoards = this.state.boards.slice(0, -1);
       this.setState({
         boards: newBoards,
-        dead: new Array(newBoards.length).fill(false),
       });
     }
   }
 
+  // Hitting "start" means that the human is passing and letting the computer move first
   startClickHandler() {
-    const newBoards = new Array(this.state.boards.length).fill("---------");
-    const newDead = new Array(this.state.boards.length).fill(false);
+    const [newBoards, winning] = computerMove(this.state.boards);
     this.setState({
       boards: newBoards,
-      dead: newDead,
+      stats: {
+        humanWins: this.state.stats.humanWins,
+        computerWins: this.state.stats.computerWins,
+        winning: winning,        
+      }
     });
   }
 
+  // Hitting "quit" means conceding victory to the computer.  The "quit" button will change color
+  // to signal when a human win is no lonfger possible!
   quitClickHandler() {
     const newBoards = new Array(this.state.boards.length).fill("---------");
-    const newDead = new Array(this.state.boards.length).fill(false);
-    const computerWins = this.state.computerWins;
+    let computerWins = this.state.stats.computerWins;
+    if (!isGameOver(this.state.boards)) {
+      computerWins++;
+    }
     this.setState({
       boards: newBoards,
-      dead: newDead,
-      moveNumber: 0,
-      computerWins: computerWins + 1,
+      stats: {
+        humanWins: this.state.stats.humanWins,
+        computerWins: computerWins,
+        winning: false,
+        gameOver: false,
+      }
     });
   }
   
   bdClickHandler(event, idx) {
-    const squareNum = Math.floor(event.nativeEvent.offsetX / 63) + 3 * (Math.floor(event.nativeEvent.offsetY / 63));
-    this.mkMove(idx, squareNum, this.state.moveNumber % 2 === 0 ? "x" : "X");
-  }
-
-  mkMove(idx, square, symbol) {
-    const oldBoard = this.state.boards[idx];
-    if (oldBoard[square] !== "-") {
+    const squareNum = Math.floor(event.nativeEvent.offsetX / SQUARE_WITH_BDY) + 3 * (Math.floor(event.nativeEvent.offsetY / SQUARE_WITH_BDY));
+    let newBoards = humanMove(this.state.boards, idx, squareNum);
+    // click was not legal
+    if (newBoards === null) {
       return;
     }
-    const moveNumber = this.state.moveNumber + 1;
-    const newBoards = this.state.boards.slice();
-    newBoards[idx] = oldBoard.substring(0, square) + symbol + oldBoard.substring(square + 1);
-    const newDead = this.state.dead.slice();
-    if (isDead(newBoards[idx])) {
-      newDead[idx] = true;
+    if (isGameOver(newBoards)) {
+      this.setState({
+        boards: newBoards,
+        stats: {
+          humanWins: this.state.stats.humanWins,
+          computerWins: 1 + this.state.stats.computerWins,
+          gameOver: true,
+        }
+      });
+      return;
     }
-    // if all the boards are dead, the last person to play loses
-    const newLoser = newDead.every(d => d) ? symbol : "-";
-    if (newLoser !== "-") {
-      console.log(`${symbol} Loses`);
-    }
+    let winning;
+    [newBoards, winning] = computerMove(newBoards);
+    if (isGameOver(newBoards)) {
+      this.setState({
+        boards: newBoards,
+        stats: {
+          humanWins: 1 + this.state.stats.humanWins,
+          computerWins: this.state.stats.computerWins,
+          gameOver: true,
+          winning: winning,
+        }
+      });
+      return;
+    }    
     this.setState({
       boards: newBoards,
-      moveNumber: moveNumber,
-      dead: newDead,
-      loser: newLoser,
+      stats: {
+        humanWins: 1 + this.state.stats.humanWins,
+        computerWins: this.state.stats.computerWins,
+        gameOver: false,
+        winning: winning,
+      }      
     });
+
   }
 
   render() {
-    const boardCanvases = this.state.boards.map((elt, idx) => this.state.dead[idx] ? 
+    const alert = this.state.error ?
+      <Alert variant="danger">{this.state.error.message}</Alert> : null;
+    const boardCanvases = this.state.boards.map((elt, idx) => isDead(this.state.boards[idx]) ? 
         <Board key={idx} contents={elt} className={"boards dead"} />
       :
         <Board key={idx} contents={elt} className={"boards"} onClick={(event) => this.bdClickHandler(event, idx)}/>
       );
-    const buttons = (this.state.moveNumber === 0 ? 
+    const buttonLabel = this.state.stats.gameOver ? "Play Again" :  "Quit";
+    const buttonVariant = (this.state.stats.winning && ! this.state.stats.gameOver) ? "outline-danger" : "outline-primary";
+    const buttons = (isStart(this.state.boards) ? 
       <div>
-        <StartButton startLabel={"Start"} onClick={() => this.startClickHandler()}/>
+        <StartButton startLabel={"Start"} onClick={() => this.startClickHandler()} variant="outline-primary"/>
         <PlusButton onClick={() => this.plusClickHandler()} />
         <MinusButton onClick={() => this.minusClickHandler()} />
       </div>
       :
       <div>
-        <StartButton startLabel={"Quit"} onClick={() => this.quitClickHandler()}/>
+        <StartButton startLabel={buttonLabel} variant={buttonVariant} onClick={() => this.quitClickHandler()}/>
       </div>);
     return (
         <div>
+          {alert}
           <div className="game-board">
             <a href="http://www.hurd-sullivan.com/"><img id="hslogo" src="images/HS.gif" alt="Go to Hurd-Sullivan.com"/></a>
             {buttons}
-            <Scores humanWins={this.state.humanWins} computerWins={this.state.computerWins}/>
+            <Scores humanWins={this.state.stats.humanWins} computerWins={this.state.stats.computerWins}/>
             <div>
               {boardCanvases}
             </div>
@@ -169,7 +225,7 @@ class Game extends React.Component {
               <li>Red and Blue take turns playing X's</li>
               <li>A board with three X's in a row (irrespective of color) is dead</li>
               <li>Whoever kills the last board, loses</li>
-              <li>Press "start" to make the computer move first, or click on a square</li>
+              <li>Press "Start" to make the computer move first, or click on a square</li>
               <li><a href="./discussion.html">More discussion</a></li>
             </ul>
           </div>
@@ -177,26 +233,6 @@ class Game extends React.Component {
     );    
   }
 }
-
-function isDead(board) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let line of lines) {
-    const [a, b, c] = line;
-    if ((board.charAt(a) !== "-") && (board.charAt(b) !== "-") && (board.charAt(c) !== "-")) {
-      return true;
-    }
-  }
-  return false;
-}  
 
 //Line drawing  ========================================
 
@@ -212,23 +248,22 @@ function drawBoard(c) {
   context2D.strokeStyle = 'black';
   context2D.globalAlpha = 1.0;
   context2D.clearRect(0, 0, c.width, c.height);
-  context2D.lineWidth = 3;
-  drawLine(context2D, 60, 0, 60, 185);
-  drawLine(context2D, 123, 0, 123, 185);
-  drawLine(context2D, 0, 60, 185, 60);
-  drawLine(context2D, 0, 123, 185, 123);
+  context2D.lineWidth = LINE_WIDTH;
+  drawLine(context2D, SQUARE_WIDTH, 0, SQUARE_WIDTH, CANVAS_SIZE - 1);
+  drawLine(context2D, 2 * SQUARE_WIDTH + LINE_WIDTH, 0, 2 * SQUARE_WIDTH + LINE_WIDTH, CANVAS_SIZE - 1);
+  drawLine(context2D, 0, SQUARE_WIDTH, CANVAS_SIZE - 1, SQUARE_WIDTH);
+  drawLine(context2D, 0, 2 * SQUARE_WIDTH + LINE_WIDTH, CANVAS_SIZE - 1, 2 * SQUARE_WIDTH + LINE_WIDTH);
 }
 
 function drawX(c, moveIdx, clr) {
   const context2D = c.getContext('2d');
-  // context2D.strokeStyle = ['red', 'blue'][moveNumber % 2];
   context2D.strokeStyle = clr;
   const col = moveIdx % 3;
   const row = Math.floor((moveIdx % 9) / 3);  
   const x = 7;
   const y = 59 - x;
-  drawLine(context2D, x + 63 * col, x + 63 * row, y + 63 * col, y + 63 * row);
-  drawLine(context2D, x + 63 * col, y + 63 * row, y + 63 * col, x + 63 * row);
+  drawLine(context2D, x + SQUARE_WITH_BDY * col, x + SQUARE_WITH_BDY * row, y + SQUARE_WITH_BDY * col, y + SQUARE_WITH_BDY * row);
+  drawLine(context2D, x + SQUARE_WITH_BDY * col, y + SQUARE_WITH_BDY * row, y + SQUARE_WITH_BDY * col, x + SQUARE_WITH_BDY * row);
 }
 
 
