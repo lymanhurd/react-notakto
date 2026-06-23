a0cebeabd8a2a31e1889ccd5edb3c2add67cb2fd
Lyman Hurd <lyman.hurd@gmail.com>
2026-06-22 22:01:44 -0400

    add version

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import Game from './Game.js'
+import Game from './Game.tsx'
+import version from "./version.json";
 
 export default function App() {
   return (
     <div className="app-container">
       <Game />
+
+      <div style={{ marginTop: "1rem", opacity: 0.6, fontSize: "0.8rem" }}>
+        Build {version.build} • {version.commit.slice(0, 7)}
+      </div>
     </div>
   )
 }
